84dc4576634f47eca3b4a3b24769ac706bcc0658
Josh <joshuafloh@gmail.com>
2017-09-18 16:06:55 +0930

    Added basic command parsing, commands are parsed onto a string array, right now only prefix checking is implemented.

--- src/main/java/rexr/rexrbot/App.java
+++ src/main/java/rexr/rexrbot/App.java
@@ -1,5 +1,7 @@
 package rexr.rexrbot;
 
+import java.util.StringTokenizer;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.core.AccountType;
@@ -16,14 +18,15 @@ public class App extends ListenerAdapter
 {
 	public static void main(String[] args) throws LoginException, IllegalArgumentException, RateLimitedException
 	{
-		JDA api = new JDABuilder(AccountType.BOT)
-				.setToken(BotToken.getToken()).buildAsync();
+		JDA api = new JDABuilder(AccountType.BOT).setToken(BotToken.getToken()).buildAsync();
 		api.addEventListener(new App());
 	}
 
 	@Override
 	public void onMessageReceived(MessageReceivedEvent e)
 	{
+		String prefix = "@RexrBot";
+		
 		Message message = e.getMessage();
 		MessageChannel channel = e.getChannel();
 		User user = e.getAuthor();
@@ -31,9 +34,24 @@ public class App extends ListenerAdapter
 		// Do not respond to messages from other bots, including ourself
 		if (user.isBot())
 			return;
+
+		// Parse the command & store it in an array
+		String command = message.getContent();
+		StringTokenizer st = new StringTokenizer(command);
+		int numArgs = st.countTokens();
+		String[] args = new String[numArgs];
 		
-		// Parse the command
+		for (int i = 0; i < numArgs; i++)
+		{
+			args[i] = st.nextToken();
+		}
 		
-		channel.sendMessage("Hello, " + user.getAsMention() + ", you said: " + message.getContent()).queue();
+		// Check for the correct prefix
+		if (args[0].equals(prefix))
+		{
+			// do stuff
+			channel.sendMessage("Prefix correctly used.").queue();
+		}
+		// else do nothing
 	}
 }
